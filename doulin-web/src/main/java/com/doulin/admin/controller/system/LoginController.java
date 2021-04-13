9f5e5d34a14a8b9ed5a191f51dc9ab6a4edce7d0
15638836857 <15638836857@163.com>
2021-04-13 10:16:24 +0800

    缓存配置

---
+++ doulin-web/src/main/java/com/doulin/admin/controller/system/LoginController.java
@@ -0,0 +1,18 @@
+package com.doulin.admin.controller.system;
+
+import com.doulin.admin.controller.common.BaseWebController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @className 系统登录控制器类
+ * @Description TODO
+ * @Autor 马凌冰
+ * @Date 2021/4/13 10:10
+ * @Version 1.0
+ */
+@RestController
+@RequestMapping("/login")
+public class LoginController extends BaseWebController {
+
+}
